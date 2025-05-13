a1e3fff52f13fb85a01e283d3a02e3b6aa9b00ef
Hoàng Văn Toàn <hoangdang77bg@gmail.com>
2025-05-13 15:12:22 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/components/header/header.component';
+import { FooterComponent } from './components/components/footer/footer.component';
+import { HomeComponent } from './components/components/home/home.component';
+import { ProductListComponent } from './components/components/products/product-list/product-list.component';
+import { ProductDetailComponent } from './components/components/products/product-detail/product-detail.component';
+import { StoreLocatorComponent } from './components/components/store-locator/store-locator.component';
+import { AboutComponent } from './components/components/about/about.component';
+import { ContactComponent } from './components/components/contact/contact.component';
+import { GalleryComponent } from './components/components/gallery/gallery.component';
+import { TechnologyComponent } from './components/components/technology/technology.component';
+import { SupportComponent } from './components/components/support/support.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    StoreLocatorComponent,
+    AboutComponent,
+    ContactComponent,
+    GalleryComponent,
+    TechnologyComponent,
+    SupportComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
